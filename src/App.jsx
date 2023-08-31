6af20d36745164fa08d129231b80e406c9203a05
eduardsdn <eduard.sdn@gmail.com>
2023-08-31 14:38:07 +0200

    Calculator component responsiveness improved

--- src/App.jsx
+++ src/App.jsx
@@ -128,10 +128,3 @@ export default App;
 
 
 
-{/* <div className={AppCSS.limitationsCards}>
-                <LimitationsCard limitationsCardIcon={limitationsGenderImg} limitationsCardTitle={"Gender"} limitationsCardText={"The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI."}/>
-                <LimitationsCard limitationsCardIcon={limitationsAgeImg} limitationsCardTitle={"Age"} limitationsCardText={"In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content."}/>
-                <LimitationsCard limitationsCardIcon={limitationsMuscleImg} limitationsCardTitle={"Muscle"} limitationsCardText={"BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."}/>
-                <LimitationsCard limitationsCardIcon={limitationsPregnancyImg} limitationsCardTitle={"Pregnancy"} limitationsCardText={"Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."}/>
-                <LimitationsCard limitationsCardIcon={limitationsRaceImg} id={AppCSS.hey}limitationsCardTitle={"Race"} limitationsCardText={"Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse."}/>
-              </div> */}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
             <img src={logo} className={HeroCSS.logo} alt="" />
             <h1 className={HeroCSS.title}>Body Mass <br /> Index Calculator</h1>
             <p className={HeroCSS.text}>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
-            {/* <Calculator></Calculator> */}
+            <Calculator></Calculator>
         </div>
     )
 }
